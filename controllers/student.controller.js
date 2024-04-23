83af60360010e5e695a637bf29aa5b7b5188b125
mhxii <mhxii105@gmail.com>
2024-04-23 12:13:05 +0000

    g

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -46,3 +46,37 @@ exports.findAll = (req, res) => {
       });
     });
 };
+
+// Find a single Student with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+  Student.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Student with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Student with id=" + id });
+    });
+};
+
+// Update a Student by the id in the request
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!",
+    });
+  }
+  const id = req.params.id;
+  Student.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Student with id=${id}. Maybe Student was not found!`,
+        });
+      } else res.send({ message: "Stu"});
+    });
+};
